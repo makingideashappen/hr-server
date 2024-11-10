8ec0eff7c13feecce5b4965761aee541914859e2
DESKTOP-7TJI3IV\DELL <makingideashappenreal@gmail.com>
2024-11-11 00:14:52 +0100

    no message

--- guestbook/guestbook/settings.py
+++ guestbook/guestbook/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.getenv(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DJANGO_DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "humancause.netlify.app").split(
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "*").split(
     ","
 )  # Render will pass this value
 
